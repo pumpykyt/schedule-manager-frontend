512ef8e707417236056809ea094a4901a6a51c29
veremeychukden <veremeychukden@gmail.com>
2022-05-29 23:47:43 +0300

    Update homepage and navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,68 +1,13 @@
-import { CgProfile } from 'react-icons/cg';
-import { MdLeaderboard } from 'react-icons/md';
-import { MdGrid4X4 } from 'react-icons/md';
-import { MdLogout } from 'react-icons/md';
-import { MdLogin } from 'react-icons/md';
-import {useState} from "react";
-import {Modal} from "antd";
-import { useLoginUserMutation } from "../apis/authApi";
-import {useDispatch} from "react-redux";
-import {setIsAdmin, setIsAuthenticated} from "../slices/authSlice";
-import jwtDecode from "jwt-decode";
-
 const Nav = props => {
-    const [showLoginModal, setShowLoginModal] = useState(false);
-    const [loginModel, setLoginModel] = useState(null);
-    const [login, { isLoading, isSuccess }] = useLoginUserMutation();
-    const dispatch = useDispatch();
-
-    const handleLogin = () => {
-        login(loginModel)
-        .then(result => {
-            console.log(result);
-            dispatch(setIsAuthenticated(true));
-            dispatch(setIsAdmin(jwtDecode(result.data.token).role === 'admin'));
-            localStorage.setItem('token', result.data.token);
-        })
-        .catch(error => console.log(error));
-    }
-
 
     return(
         <div className="nav w-full fixed">
-            <Modal footer={null} bodyStyle={{background: '#131313'}} visible={showLoginModal} onCancel={() => setShowLoginModal(false)}>
-                <div className="px-12 py-6 rounded">
-                    <h1 className="text-2xl font-semibold text-white mb-5">Sign-in</h1>
-                    <input placeholder="Email"
-                           onChange={e => setLoginModel({...loginModel, email: e.target.value})}
-                           type="text"
-                           className="bg-gray-300 mb-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    />
-                    <input placeholder="Password"
-                           onChange={e => setLoginModel({...loginModel, password: e.target.value})}
-                           type="password"
-                           className="bg-gray-300 mb-5 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    />
-                    <button onClick={handleLogin} className="px-5 py-2 drop-shadow-2xl rounded-lg bg-indigo-900 text-white">Submit</button>
-                </div>
-            </Modal>
-            <div className="grid grid-cols-10 py-3 container mx-auto items-center">
-                <div className="col-span-2 text-white text-2xl font-semibold cursor-pointer">Schedule manager</div>
-                <div className="col-span-4"></div>
-                <div className="text-white text-2xl mx-auto text-center cursor-pointer">
-                    <CgProfile/>
-                    <span/>
-                </div>
-                <div className="text-white text-2xl mx-auto text-center cursor-pointer">
-                    <MdGrid4X4/>
-                </div>
-                <div className="text-white text-2xl mx-auto text-center cursor-pointer">
-                    <MdLeaderboard/>
-                </div>
-                <div className="text-white text-2xl mx-auto text-center cursor-pointer">
-                    <MdLogout/>
-                    <MdLogin onClick={() => setShowLoginModal(true)}/>
-                </div>
+            <div className="flex flex-row flex-wrap-nowrap justify-between container mx-auto gap-x-20 items-center py-3">
+                <p className="text-white text-2xl">Schedule manager</p>
+                <div className="flex-1"></div>
+                <p className="text-white font-light text-xl cursor-pointer">home</p>
+                <p className="text-white font-light text-xl cursor-pointer">schedules</p>
+                <p className="text-white font-light text-xl cursor-pointer">my account</p>
             </div>
         </div>
     );
--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,27 +1,18 @@
-import Spline from "@splinetool/react-spline";
 import Fade from 'react-reveal/Fade';
+import girlImg from '../img/girl.png';
 
 const HomePage = () => {
     return(
-        <div className="home-page min-h-screen bg-gray-900">
-            <div className="grid grid-cols-2 h-screen items-center container mx-auto">
-                <div className="grid grid-cols-1">
-                    <Fade left delay={1400}>
-                        <h1 className="text-white text-left text-3xl mb-10">Welcome to schedule manager</h1>
-                    </Fade>
-                    <Fade left delay={1500}>
-                        <p className="text-white text-left w-3/4 text-5xl mb-10">
-                            Journey into comfortable timetables
-                        </p>
-                    </Fade>
-                    <Fade left delay={1600}>
-                        <div className="w-full">
-                            <button className="float-left text-white text-lg border-2 px-5 py-3 rounded-lg">Get started</button>
-                        </div>
+        <div className="home-page min-h-screen bg-bg-image bg-no-repeat bg-cover">
+            <div className="container mx-auto grid grid-cols-2 items-center min-h-screen">
+                <div className="text-content">
+                    <Fade delay={500} duration={1000} left>
+                        <p className="text-white text-7xl font-bold mb-5">Smart schedule manager app</p>
+                        <p className="text-white text-2xl font-light">It's easy not to be late for lessons.</p>
                     </Fade>
                 </div>
-                <Fade delay={1300}>
-                    <Spline scene="https://prod.spline.design/sfnraVlBAMhqGxdl/scene.splinecode"/>
+                <Fade delay={500} duration={1000}>
+                    <img src={girlImg} alt="girl" className="justify-self-center w-3/4"/>
                 </Fade>
             </div>
         </div>
