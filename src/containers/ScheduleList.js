dc03f7b8636665f3d4d6b9b74c7abe581cf2e2f4
aesense <senseandrey@gmail.com>
2022-05-30 03:37:03 +0300

    Add breadcrumbs

--- src/containers/ScheduleList.js
+++ src/containers/ScheduleList.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useGetSchedulesQuery } from "../apis/scheduleApi";
-import { Fade } from 'react-reveal';
 import Paginator from "../components/Paginator";
 import Schedule from "../components/Schedule";
 import { Select } from 'antd';
+import useReactRouterBreadcrumbs from "use-react-router-breadcrumbs";
 const { Option } = Select;
 
 function ScheduleList(){
@@ -12,34 +12,61 @@ function ScheduleList(){
     const [pageSize, setPageSize] = useState(12);
     const [currentPage, setCurrentPage] = useState(1);
     const { data, error, isLoading } = useGetSchedulesQuery({ pageNumber: currentPage, pageSize, search, sort });
+    const breadcrumbs = useReactRouterBreadcrumbs();
+    const paginator = useMemo(() => 
+        !isLoading && !error &&
+        <Paginator pageSize={pageSize} 
+                   setPageSize={setPageSize} 
+                   currentPage={currentPage} 
+                   setCurrentPage={setCurrentPage} 
+                   totalCount={data.totalDataCount}
+        />, [data, error, isLoading, currentPage, pageSize]
+    );
     
     return(
-        <div className="schedule-list min-h-screen bg-bg-image bg-cover bg-no-repeat pt-24">
-            <Fade delay={200}>
-                <div className="container mx-auto w-full mb-5">
-                    <input value={search} onChange={e => setSearch(e.target.value)} placeholder="Search" className="mr-5 shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"/>
-                    <Select defaultValue={sort} onChange={value => setSort(value)} style={{ width: 200 }}>
+        <div className="schedule-list min-h-screen bg-bg-image bg-cover bg-no-repeat pt-36">
+            <div className="container mx-auto grid grid-cols-12 gap-x-5">
+                <div className="col-span-12 flex gap-1 mb-5">
+                    {
+                        breadcrumbs.map(({ breadcrumb }) => (
+                            <div className="text-white text-lg">
+                                {breadcrumb} / 
+                            </div>
+                        ))
+                    }
+                </div>
+                <div className="col-span-12 text-white text-4xl font-bold">
+                    List of available schedules
+                </div>
+                <div className="col-span-12 justify-self-end">
+                    <Select defaultValue={sort} onChange={value => setSort(value)}>
                         <Option value="name_asc">Sort by name ascending</Option>
                         <Option value="name_desc">Sort by name descending</Option>
                     </Select>
                 </div>
-            </Fade>
-            <div className="container mx-auto mb-5">
-                { 
-                    !isLoading && !error && 
-                    <div className="grid grid-cols-6 gap-10">
+                <div className="col-span-12 bg-indigo-800 h-0.5 my-5"></div>
+                <div className="w-full mb-5 bg-indigo-900 p-5 rounded-xl col-span-2 min-h-full">
+                    <input value={search} onChange={e => setSearch(e.target.value)} placeholder="Search" className="mr-5 shadow appearance-none border rounded py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"/>
+                </div>
+                <div className="col-span-10">
+                    { 
+                        !isLoading && !error && 
+                        <div className="grid grid-cols-6 gap-10 mb-5">
+                            {
+                                data.data.map((element) => (<Schedule key={element.id} id={element.id} name={element.name}/>))
+                            }
+                        </div>
+                    }
+                    {
+                    !isLoading && !error &&
+                    <div className="container mx-auto">
                         {
-                            data.data.map((element) => (<Schedule key={element.id} id={element.id} name={element.name}/>))
+                            paginator
                         }
                     </div>
-                }
-            </div>
-            {
-                !isLoading && !error &&
-                <div className="container mx-auto">
-                    <Paginator pageSize={10} setPageSize={setPageSize} currentPage={currentPage} setCurrentPage={setCurrentPage} totalCount={data.totalDataCount}/>
+                    }
                 </div>
-            }
+            </div>
         </div>
     );
 }
